acdc8d155a6f87832adc21586abf5895d452e505
Pedro Rey Anca <pedroreya@gmail.com>
2021-05-23 02:36:06 +0200

    Switch to version 0.0.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
+VERSION = '0.0.2' 
 DESCRIPTION = 'Python API wrapper for bus.gal'
 with open("README.md", "r", encoding="utf-8") as fh:
     LONG_DESCRIPTION = fh.read()
