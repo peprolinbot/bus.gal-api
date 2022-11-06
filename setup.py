f6487b735460d723d14c9d91548410c34ce763de
Pedro Rey Anca <personal@peprolinbot.com>
2022-11-06 20:26:52 +0100

    Update version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.5.1' 
+VERSION = '0.0.6' 
 DESCRIPTION = 'Python API wrapper for bus.gal'
 with open("README.rst", "r", encoding="utf-8") as fh:
     LONG_DESCRIPTION = fh.read()
