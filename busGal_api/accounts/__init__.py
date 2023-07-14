32cb618f63fab22bd995d7a2d03f65c64a27825b
peprolinbot <personal@peprolinbot.com>
2023-07-14 20:25:28 +0200

    Add accounts example

--- busGal_api/accounts/__init__.py
+++ busGal_api/accounts/__init__.py
@@ -1,3 +1,26 @@
+"""
+# Introduction
+
+This submodule takes care of accounts and other stuff which requires logging in. This is all reverse-engineered using [mitmproxy](https://mitmproxy.org) and other techniques on the [Android app](https://play.google.com/store/apps/details?id=gal.xunta.transportepublico).
+
+# Quick example
+
+This is just a simple command-line "client" which shows your cards and makes you rename them:
+
+``` python
+from busGal_api import accounts as api
+
+print("Please login to continue")
+account=api.Account(input("Email: "), input("Password: "))
+
+print("Here are your cards. You are going to rename each of them")
+cards=account.get_cards()
+for card in cards:
+    print(card)
+    card.rename(input("New name: "))
+```
+"""
+
 from ..rest_adapter import RestAdapter as RestAdapter
 from ..known_servers import XG_APP as BASE_URL
 
