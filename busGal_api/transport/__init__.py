c2a1436623fabe6bc92f8514f72e9a211f1bce28
Pedro Rey Anca <personal@peprolinbot.com>
2023-08-31 14:57:08 +0200

    Remove public API due to its unreliability
    This doesn't mean the non-public one is great or anything ;)

--- busGal_api/transport/__init__.py
+++ busGal_api/transport/__init__.py
@@ -15,7 +15,7 @@ This is also this submodule's `__main__.py`, therefore you can `python -m busGal
 """
 
 from ..rest_adapter import RestAdapter as RestAdapter
-from ..known_servers import XG_EXTERNOS as BASE_URL
+from ..known_servers import XG_APP as BASE_URL
 
 _rest_adapter = RestAdapter(BASE_URL)
 
