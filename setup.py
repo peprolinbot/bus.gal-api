76a3758d4ea216fdac0540660842aef8c5d886f7
Pedro Rey Anca <personal@peprolinbot.com>
2024-07-05 00:02:20 +0200

    Add missing dependencies

--- setup.py
+++ setup.py
@@ -1,29 +1,31 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.2.0.0' 
+VERSION = '0.2.0.0'
 DESCRIPTION = 'Python API wrapper for the galician public transport'
 with open("README.md", "r", encoding="utf-8") as fh:
     LONG_DESCRIPTION = fh.read()
 
 setup(
-        name="busGal_api", 
-        version=VERSION,
-        author="Pedro Rey Anca",
-        author_email="contact@peprolinbot.com",
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        packages=find_packages(),
-        install_requires=["requests>=2.25.1"],
-        url="https://github.com/peprolinbot/bus.gal-api",
-        project_urls={
-            "Bug Tracker": "https://github.com/peprolinbot/bus.gal-api/issues",
-            "Documentation": "https://busgal-api.readthedocs.io/en/latest/",
-        },
-        keywords=['bus', 'buses', 'transport', 'public transport', 'galicia', 'api'],
-        classifiers= [
-            "Development Status :: 4 - Beta",
-            "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-            "Programming Language :: Python :: 3",
-            "Topic :: Software Development :: Libraries :: Python Modules"
-        ]
+    name="busGal_api",
+    version=VERSION,
+    author="Pedro Rey Anca",
+    author_email="contact@peprolinbot.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["requests>=2.25.1",
+                      "python-dateutil>=2.9.0.post0", "pycryptodome>=3.20.0"],
+    url="https://github.com/peprolinbot/bus.gal-api",
+    project_urls={
+        "Bug Tracker": "https://github.com/peprolinbot/bus.gal-api/issues",
+        "Documentation": "https://busgal-api.readthedocs.io/en/latest/",
+    },
+    keywords=['bus', 'buses', 'transport',
+              'public transport', 'galicia', 'api'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
 )
