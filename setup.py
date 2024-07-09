baae5b3456e0b11a7ca04df18948b387240684b3
Pedro Rey Anca <personal@peprolinbot.com>
2024-07-09 13:02:29 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.2.0.0'
+VERSION = '0.2.0.1'
 DESCRIPTION = 'Python API wrapper for the galician public transport'
 with open("README.md", "r", encoding="utf-8") as fh:
     LONG_DESCRIPTION = fh.read()
