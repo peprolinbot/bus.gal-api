f731398049cd9d758ea3a37c7181e9f6ac231eb0
Pedro Rey Anca <contact@peprolinbot.com>
2024-09-26 16:43:30 +0200

    Add new real-time api used in the app (SIRI)

--- busGal_api/known_servers.py
+++ busGal_api/known_servers.py
@@ -12,3 +12,8 @@ XG_XNQR_APP = "https://xentenovaqr.xunta.gal/api"
 """
 The API the Android app uses for the Xente Nova QR functionality
 """
+
+XG_REALTIME_APP = "https://sae-its.xunta.gal:8019"
+"""
+The SOAP API the Android app uses
+"""
